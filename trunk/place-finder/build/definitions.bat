9e8b7370181e9aba4cef445a7bc971f9d1222fba
alistair.rutherford <alistair.rutherford@1aab7638-d374-11dd-8179-53c65a4d6d95>
2009-01-18 14:58:23 +0000

    - Updated font for geocoder screen which was too small (discovered this by actually running it on G1 phone).

--- trunk/place-finder/build/definitions.bat
+++ trunk/place-finder/build/definitions.bat
@@ -1,1 +1,3 @@
-set DEBUG_KEYSTORE=C:\Documents and Settings\Admin\Local Settings\Application Data\Android\debug.keystore
+set TOOLS_DIR=C:/Development/java/android/sdk/android-sdk-windows-1.0_r2/tools
+set DEBUG_KEYSTORE=C:/Documents and Settings/Admin/Local Settings/Application Data/Android/debug.keystore
+set RELEASE_KEYSTORE=C:/Development/java/android/sdk/keystore/place-finder/place-finder-release-key.keystore
--- trunk/place-finder/src/com/netthreads/android/geocode/R.java
+++ trunk/place-finder/src/com/netthreads/android/geocode/R.java
@@ -10,6 +10,9 @@ package com.netthreads.android.geocode;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int geocoder_list_font_size=0x7f040000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int screen_background_black=0x7f020001;
@@ -17,17 +20,17 @@ public final class R {
         public static final int transparent_background=0x7f020003;
     }
     public static final class id {
-        public static final int app_version=0x7f070001;
-        public static final int edit=0x7f070002;
-        public static final int layout_zoom=0x7f070008;
-        public static final int location=0x7f070004;
-        public static final int location_name=0x7f070006;
-        public static final int main=0x7f070005;
-        public static final int map_view=0x7f070007;
-        public static final int show_about=0x7f07000a;
-        public static final int show_geocoder=0x7f070009;
-        public static final int text=0x7f070000;
-        public static final int typed=0x7f070003;
+        public static final int app_version=0x7f080001;
+        public static final int edit=0x7f080002;
+        public static final int layout_zoom=0x7f080008;
+        public static final int location=0x7f080004;
+        public static final int location_name=0x7f080006;
+        public static final int main=0x7f080005;
+        public static final int map_view=0x7f080007;
+        public static final int show_about=0x7f08000a;
+        public static final int show_geocoder=0x7f080009;
+        public static final int text=0x7f080000;
+        public static final int typed=0x7f080003;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -36,34 +39,34 @@ public final class R {
         public static final int main=0x7f030003;
     }
     public static final class menu {
-        public static final int main_activity_menu=0x7f060000;
+        public static final int main_activity_menu=0x7f070000;
     }
     public static final class string {
-        public static final int about=0x7f040007;
-        public static final int activity_about=0x7f040003;
-        public static final int activity_geocode=0x7f040002;
-        public static final int app_name=0x7f040000;
-        public static final int author=0x7f040004;
-        public static final int copyright=0x7f040005;
-        public static final int current_location=0x7f040001;
-        public static final int find=0x7f040008;
-        public static final int main_no_items=0x7f040006;
+        public static final int about=0x7f050007;
+        public static final int activity_about=0x7f050003;
+        public static final int activity_geocode=0x7f050002;
+        public static final int app_name=0x7f050000;
+        public static final int author=0x7f050004;
+        public static final int copyright=0x7f050005;
+        public static final int current_location=0x7f050001;
+        public static final int find=0x7f050008;
+        public static final int main_no_items=0x7f050006;
     }
     public static final class style {
         /**  Base application theme is the default theme. 
          */
-        public static final int Theme=0x7f050000;
+        public static final int Theme=0x7f060000;
         /**  A theme that has a translucent background.  Here we explicitly specify
          that this theme is to inherit from the system's translucent theme,
          which sets up various attributes correctly.. 
          */
-        public static final int Theme_Translucent=0x7f050001;
+        public static final int Theme_Translucent=0x7f060001;
         /**  Variation on our application theme that has a transparent
          background; this example completely removes the background,
          allowing the activity to decide how to composite.  Also here we
          force the translucency ourself rather than making use of the built-in
          translucent theme. 
          */
-        public static final int Theme_Transparent=0x7f050002;
+        public static final int Theme_Transparent=0x7f060002;
     }
 }
