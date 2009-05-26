ff88a726478fa9c7e7530228a9d8ceeb507ba3b4
alistair.rutherford <alistair.rutherford@1aab7638-d374-11dd-8179-53c65a4d6d95>
2009-05-26 08:55:58 +0000

    - Updated GL screen for super-fast buffer draw.

--- trunk/bulletml/src/com/netthreads/android/bulletml/graphics/ScreenGL.java
+++ trunk/bulletml/src/com/netthreads/android/bulletml/graphics/ScreenGL.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2009 Alistair Rutherford, Glasgow, Scotland, UK, www.netthreads.co.uk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
 package com.netthreads.android.bulletml.graphics;
 
 import java.nio.ByteBuffer;
@@ -16,35 +31,47 @@ public class ScreenGL implements IScreen
 {
 	private GL10 surface = null;
 
-	private ByteBuffer byteBuffer = null;
+	private ByteBuffer lineBuffer = null;
+    private ByteBuffer colorBuffer = null;
 
-	private IntBuffer intBuffer = null;
+	private IntBuffer lineIntBuffer = null;
+    private IntBuffer colorIntBuffer = null;
 	
-	private float lineWidth = 1.0f;
+	private int lineWidth = 1;
 
 	private int screenWidth = 0;
 	private int screenHeight = 0;
 	
-    private int[] line = new int[] { 0,0,0,0,0,0 };
+    private static int[] line = null;
+    private static int[] color = null;
 
-	/**
-	 * Construct OpenGL Screen.
-	 * 
-	 * @param lineWidth
-	 * @param screenWidth
-	 * @param screenHeight
-	 */
+    private static final int ONE = 0x10000;
+    
+    private static final int LINE_BUFFER_SIZE = 3500;
+    
+    private int vertexIndex = 0;
+    private int colorIndex = 0;
+    private int lineIndex = 0;
+
 	public ScreenGL(int lineWidth, int screenWidth, int screenHeight)
 	{
+	    line = new int[LINE_BUFFER_SIZE*2*3];  // size*points_per_line*vertice_per_point
+        color = new int[LINE_BUFFER_SIZE*2*4]; // size*points_per_line*color_value_per_point (r, g, b, a)
+	    
 		this.lineWidth = lineWidth;
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
 
 		// // Buffer
-		byteBuffer = ByteBuffer.allocateDirect(24);
-		byteBuffer.order(ByteOrder.nativeOrder());
+        lineBuffer = ByteBuffer.allocateDirect(line.length*4);
+		lineBuffer.order(ByteOrder.nativeOrder());
+        lineIntBuffer = lineBuffer.asIntBuffer();
+
+		// Color
+        colorBuffer = ByteBuffer.allocateDirect(color.length*4);
+        colorBuffer.order(ByteOrder.nativeOrder());
 		
-		intBuffer = byteBuffer.asIntBuffer();
+		colorIntBuffer = colorBuffer.asIntBuffer();
 	}
 
 	/**
@@ -56,6 +83,9 @@ public class ScreenGL implements IScreen
 	{
 		// Implement
 		surface.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		
+		// Draw buffer contents filled on last pass
+		drawBuffer();		
 	}
 
     /**
@@ -66,53 +96,92 @@ public class ScreenGL implements IScreen
      * @param y1
      * @param x2
      * @param y2
-     * @param color
+     * @param lineColor
      */
     @Override
-    public void drawLine(int x1, int y1, int x2, int y2, int color)
+    public void drawLine(int x1, int y1, int x2, int y2, int lineColor)
     {
-        line[0] = x1<<16;
-        line[1] = (screenHeight - y1)<<16;
-        line[2] = 0;
-        line[3] = x2<<16;
-        line[4] = (screenHeight - y2)<<16;
-        line[5] = 0;
-        
-        intBuffer.clear(); // Workaround
-        intBuffer.compact(); // Workaround
-        
-        intBuffer.put(line);
-        intBuffer.position(0);
-        
-        surface.glVertexPointer(3, GL10.GL_FIXED, 0, intBuffer);
-        
-        surface.glEnableClientState(GL10.GL_VERTEX_ARRAY); // Enable use of  vertex array
+        fillBuffer(x1, y1, x2, y2, lineColor);
 
-        /*
-        private static final float PERCENT_CONVERSION = 1.0f/255.0f;
-        
-        // Colour values are passed as percentages.
-        float red = PERCENT_CONVERSION*((color >> 16) & 0xFF);
-        float green = PERCENT_CONVERSION*((color >> 8) & 0xFF);
-        float blue = PERCENT_CONVERSION*((color & 0xFF));
-
-        surface.glColor4f(red, green, blue, 0f);
-        */
-
-        // Colour values are passed as percentages.
-        // Note: We might get even faster if we put this stuff in a lookup table.
-        int red = (color&0x00FF0000);
-        int green = (color&0x0000FF00)<<8;
-        int blue = (color&0x000000FF)<<16;
-
-        surface.glColor4x(red/255, green/255, blue/255, 0);
-        
-        surface.glLineWidth(lineWidth);
-
-        surface.glDrawArrays(GL10.GL_LINES, 0, 2);
-
-        surface.glDisableClientState(GL10.GL_VERTEX_ARRAY); // Disable use of  vertex array
+        lineIndex++;
     }
+
+    /**
+     * Draw line buffer contents filled on the last pass through the data.
+     * 
+     */
+    private void drawBuffer()
+    {
+        if (lineIndex>0)
+        {
+            // Reset
+            
+            // UNCOMMENT THESE TO RUN ON THE EMULATOR. CRASHES ON THE PHONE
+//            lineIntBuffer.clear(); // Workaround
+//            lineIntBuffer.compact(); // Workaround
+//            colorIntBuffer.clear(); // Workaround
+//            colorIntBuffer.compact(); // Workaround
+            
+            lineIntBuffer.put(line);
+            lineIntBuffer.position(0);
+            
+            colorIntBuffer.put(color);
+            colorIntBuffer.position(0);
+            
+            surface.glVertexPointer(3, GL10.GL_FIXED, 0, lineIntBuffer);
+            surface.glColorPointer(4, GL10.GL_FIXED, 0, colorIntBuffer);
+            
+            surface.glEnableClientState(GL10.GL_VERTEX_ARRAY); // Enable vertex array
+            surface.glEnableClientState(GL10.GL_COLOR_ARRAY);  // Enable colour array   
+            
+            surface.glLineWidthx(lineWidth);
+
+            surface.glDrawArrays(GL10.GL_LINES, 0, lineIndex*2);
+    
+            surface.glDisableClientState(GL10.GL_VERTEX_ARRAY); // Disable use of  vertex array
+            surface.glDisableClientState(GL10.GL_COLOR_ARRAY); // Disable use of  colour array
+            
+            // Reset our counters
+            vertexIndex = 0;
+            colorIndex = 0;
+            lineIndex = 0;
+        }
+        
+    }
+    /**
+     * Fill draw buffer.
+     * 
+     * @param x1
+     * @param y1
+     * @param x2
+     * @param y2
+     * @param lineColor
+     */
+    private void fillBuffer(int x1, int y1, int x2, int y2, int lineColor)
+    {
+        int red = ((lineColor&0x00FF0000))/255;
+        int green = ((lineColor&0x0000FF00)<<8)/255;
+        int blue = ((lineColor&0x000000FF)<<16)/255;
+        
+        line[vertexIndex++] = x1<<16;
+        line[vertexIndex++] = (screenHeight - y1)<<16;
+        line[vertexIndex++] = 0;
+
+        line[vertexIndex++] = x2<<16;
+        line[vertexIndex++] = (screenHeight - y2)<<16;
+        line[vertexIndex++] = 0;
+
+        color[colorIndex++] = red;
+        color[colorIndex++] = green;
+        color[colorIndex++] = blue;
+        color[colorIndex++] = ONE;
+        
+        color[colorIndex++] = red;
+        color[colorIndex++] = green;
+        color[colorIndex++] = blue;
+        color[colorIndex++] = ONE;
+    }
+    
 	/**
 	 * Draw bitmap to screen.
 	 * 
